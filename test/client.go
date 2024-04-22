05d35c19e0ff37ae1e2b5cf20be5e6478ed65d17
Ty-He <2291564138@qq.com>
2024-04-22 19:52:49 +0800

    zinx-demo v0.3

--- test/client.go
+++ test/client.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    conn, err := net.Dial("tcp4", "192.168.18.128:8999")
+    conn, err := net.Dial("tcp", "192.168.18.128:8999")
     if err != nil {
         fmt.Println("Connect Error")
         return 
@@ -21,13 +21,13 @@ func main() {
         }
         buf := make([]byte, 512)
         n, err := conn.Read(buf)
-        if err != nil {
-            fmt.Println("Read Error")
-            continue
-        }
         if n == 0 {
             fmt.Println("Quit Connection")
             break
+        }
+        if err != nil {
+            fmt.Println("Read Error")
+            continue
         }
         fmt.Printf("Read buf:%s, len:%d\n", buf, n)
         time.Sleep(1 * time.Second)
